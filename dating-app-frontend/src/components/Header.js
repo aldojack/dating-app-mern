9f9b3c06055f8076d42138ecc0070bc24f3a90a7
Alan Jack <60137518+aldojack@users.noreply.github.com>
2022-09-28 13:03:50 +0100

    Added dating card component to the app.js

--- dating-app-frontend/src/components/Header.js
+++ dating-app-frontend/src/components/Header.js
@@ -2,7 +2,6 @@ import './Header.css'
 import PersonIcon from '@material-ui/icons/Person'
 import IconButton from '@material-ui/core/IconButton'
 import ForumIcon from '@material-ui/icons/Forum'
-import { Icon } from '@material-ui/core'
 
 export default function Header(){
     return(
